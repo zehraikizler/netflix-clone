a2bf9dcf839ae7bee9d6e8882f8276df29ca045f
zehraikizler <zehraaikizler@gmail.com>
2022-07-15 01:31:11 +0300

    Movie comp and scroll func added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 export default function Home(){
   return (
     <div>
       <Main />
+      <Row rowID='1' title='UpComing' fetchURL={requests.requestUpcoming} />
+      <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
+      <Row rowID='3' title='Trending' fetchURL={requests.requestTrending} />
+      <Row rowID='4' title='TopRated' fetchURL={requests.requestTopRated} />
+      <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} />
     </div>
   )
 }
