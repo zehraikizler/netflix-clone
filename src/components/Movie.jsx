fb06a045da25040aad7927a4a452d8519a886d7c
zehraikizler <zehraaikizler@gmail.com>
2022-07-19 10:58:19 +0300

    user data saved

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa'
-import { useState } from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
 
 const Movie = ( { item }) => {
 
     const [like, setLike] = useState(false)
+    const [saved, setSaved] = useState(false)
+    const { user } = UserAuth()
+
+    const movieID = doc(db, 'users', `${user?.email}`);
+
+    const saveShow = async () => {
+        if(user?.email) {
+            setLike(!like);
+            setSaved(true);
+            await updateDoc(movieID, {
+                savedShows: arrayUnion({
+                    id: item.id,
+                    title: item.title,
+                    img: item.backdrop_path,
+                }),
+            })
+        } else {
+            alert('Please log in to save a movie');
+        }
+    };
 
   return (
     <>
@@ -12,7 +34,7 @@ const Movie = ( { item }) => {
             <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
             <div className='absolute top-0 left-0 w-full h-full hover:bg-black-80 opacity-0 hover:opacity-100 text-white'>
                 <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                <p>
+                <p onClick={saveShow}>
                     {like ? <FaHeart className='absolute top-4 left-4 text-gray-300' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-300' />}
                 </p>
             </div>
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import SavedShows from '../components/SavedShows'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+      <div className='w-full text-white'>
+        <img className='w-full h-[400px] object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/1ef84595-1fdb-4404-adac-15215ceeb3ae/db47460c-f9ae-4c42-a223-b0f4d3bc9832/TR-tr-20220711-popsignuptwoweeks-perspective_alpha_website_medium.jpg' />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
   )
 }
 
